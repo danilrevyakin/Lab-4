42aacb0ad4e2df1dd353fbcb3442fcf33077d3b1
danylo_revyakin <danylo.revyakin.ext@competitoor.com>
2023-01-12 18:27:13 +0200

    JQuery -> DOM

--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
 function openModal(id){
-    $('.modal').modal('show');
-    let date = $(`#${id}`).parent().find("[class*='date']").text();
-    let city =  $(`#${id}`).parent().find("[class*='city']").text();
-    $('#staticDate').val(`${city}: ${date}`);
+    document.querySelector('.modal').style.display = "block"
+    let date = document.querySelector(`#${id}`).parentNode.querySelector("[class*='date']").innerText;
+    let city = document.querySelector(`#${id}`).parentNode.querySelector("[class*='city']").innerText;
+
+    document.querySelector('#staticDate').value = `${city}: ${date}`;
 }
 
 function checkModal(){
-    let name = $('#first-name').val();
-    let surname = $('#last-name').val();
-    let amount = $('#tickets-amount').val();
-    let email = $('#email-input').val();
+    let name = document.querySelector('#first-name').value;
+    let surname = document.querySelector('#last-name').value;
+    let amount = document.querySelector('#tickets-amount').value;
+    let email = document.querySelector('#email-input').value;
 
     if(name && surname && email && amount >= 1){
-        $('.modal').modal('hide');
+        document.querySelector('.modal').style.display = "none"
     }
 }
